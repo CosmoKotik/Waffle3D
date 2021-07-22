e85a60031db2dc7427ae8f4b9f9e7cb5b113b2ad
CosmoKotik <47870598+CosmoKotik@users.noreply.github.com>
2021-07-21 20:29:49 -0400

    [ADD] Added Hierarchy + Scene/Game dock
    [ADD] Added a draggable hierarchy
    [ADD] Added a scene tab and a game tab
    [ADD] OpenGL/OpenTK Display

---
+++ Wafle3DEditor - Copy/Wafle3DEditor/Commands/FileNewCommandDefinition.cs
@@ -0,0 +1,32 @@
+﻿using Gemini.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wafle3DEditor.Commands
+{
+	[CommandDefinition]
+	public class FileNewCommandDefinition : CommandDefinition
+	{
+		public const string CommandName = "File.New";
+
+		public override string Name
+		{
+			get { return CommandName; }
+		}
+
+		public override string Text
+		{
+			get { return "_New"; }
+		}
+
+		public override string ToolTip
+		{
+			get { return "NEW"; }
+		}
+	}
+}
---
+++ Wafle3DEditor - Copy/Wafle3DEditor/Views/SceneView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using OpenTK.Graphics;
+using OpenTK.Graphics.ES30;
+using OpenTK.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wafle3DEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for SceneView.xaml
+    /// </summary>
+    public partial class SceneView : UserControl
+    {
+        public SceneView()
+        {
+            InitializeComponent();
+
+            //Scene.Invalidate();
+        }
+
+        private void Scene_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Scene.Invalidate();
+
+            GL.ClearColor(Color4.Blue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Scene.SwapBuffers();
+        }
+    }
+}
